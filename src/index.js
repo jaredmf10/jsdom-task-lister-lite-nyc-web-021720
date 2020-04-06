772d298442bafefd8bb79c3e75ce319fc38340c7
Jared Fleming <jaredmf10@gmail.com>
2020-04-06 08:59:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+let form = document.getElementById("create-task-form")
+var taskList = document.getElementById("tasks")
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    var description = document.getElementById("new-task-description").value
+    var newLi = document.createElement("li")
+    var button = document.createElement("button")
+    button.innerText = "x"
+    taskList.appendChild(newLi)
+    taskList.append(button)
+    newLi.innerText = description
+  })
+  let divArea = document.getElementById("list")
+  divArea.addEventListener("click", function(event){
+    event.preventDefault()
+    if (event.target.innerText === "x"){
+      event.target.parentElement.remove()
+      event.target.remove()
+    }
+  })
 });
